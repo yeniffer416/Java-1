b0aa662875fa809981a50ac3e8d6907e264ce99d
juaosuna <101885123+juaosuna@users.noreply.github.com>
2022-08-23 02:42:47 -0500

    plan de mejora 18-08-2022

---
+++ Videos/src/main/java/Videos4a20_sesion2/Video10.java
@@ -0,0 +1,27 @@
+
+package Videos4a20_sesion2;
+
+
+public class Video10 {
+    //tema del videoif-else con multiples condicionales
+    
+    public static void main(String[] args) {
+        
+        int TotalEpisodios = 15;
+        
+        System.out.println("\n------ Ciclo con If-else ---------");
+        
+        //if y else comprobacion
+        if (TotalEpisodios >1 && TotalEpisodios <= 10){
+            System.out.println("Es una mini Serie");
+        }else if(TotalEpisodios >10){
+            System.out.println("Es una Serie");            
+        }else if(TotalEpisodios ==1){
+            System.out.println("Es una Pelicula");            
+        }else{
+            System.out.println("Debe tener al menos un episodio");
+        }
+        
+    }
+    
+}
---
+++ Videos/src/main/java/Videos4a20_sesion2/Video18.java
@@ -0,0 +1,17 @@
+
+package Videos4a20_sesion2;
+
+public class Video18 {
+    //array
+    public static void main(String[] args) {
+        int[] episodiosDuracionArray = new int []{30,32,27,31,60};
+        
+        int DuracionTemporada = 0;
+        
+        for (int value : episodiosDuracionArray ){
+            DuracionTemporada +=value;
+        }
+        System.out.println(DuracionTemporada);
+    }
+    
+}
