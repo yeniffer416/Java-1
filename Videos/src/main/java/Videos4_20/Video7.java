58e5422e404f00ed28a40be36f3af20aaf28361f
juaosuna <jdosuna4@misena.edu.co>
2022-08-23 23:21:47 -0500

    plan de mejora 18-08-2022

---
+++ Videos/src/main/java/Videos4_20/Video7.java
@@ -0,0 +1,34 @@
+
+package Videos4_20;
+
+/**
+ *
+ * @author Juan Osuna
+ */
+public class Video7 {
+    
+    public static void main(String[] args) {
+        //Rectangular
+        int b = 15;
+        int a = 8;
+        
+        //Area
+        int rArea = b*a;
+        
+        //Perimetro
+        int rPerimetro = (2*b)+(2*a);
+        
+        //Triangulo
+        b = 5;
+        a = 3;
+        
+        //Area
+        double tArea = b * a / 2.0;
+        
+        System.out.println(rArea);
+        System.out.println(tArea);
+        System.out.println(rPerimetro);
+        
+    }
+    
+}
